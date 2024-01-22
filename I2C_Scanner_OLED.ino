e390a2016168edf0d4aa1f737b91b0ce2b1264e3
Imsaiguy <39391838+imsaiguy@users.noreply.github.com>
2024-01-21 16:29:16 -0800

    Update I2C_Scanner_OLED.ino

--- I2C_Scanner_OLED.ino
+++ I2C_Scanner_OLED.ino
@@ -8,23 +8,31 @@ displays using OLED 128x64
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Fonts/FreeMono9pt7b.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
+
 void setup() {                
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.setFont(&FreeMono9pt7b);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.setTextSize(1);
+  display.println("I2C address:");
+  display.display();
+  delay(2000); 
+  
 }
 
 void loop() {
   byte error, address;
   int nDevices; 
   display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.setTextSize(1);
-  display.println("I2C devices found:");
+  display.setCursor(0,10);
   nDevices = 0;
   for(address = 1; address < 127; address++ ) 
   { // The i2c_scanner uses the return value of
@@ -48,6 +56,18 @@ void loop() {
     }    
   }
   if (nDevices == 0) display.println("None");
+  display.display();
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(100);                        
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(100);                     
+  digitalWrite(LED_BUILTIN, LOW);   
+
+}
+
   display.display();
   delay(2000);           // wait 2 seconds for next scan  
 }
